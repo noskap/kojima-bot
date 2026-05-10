75722b93fd82a15519c758d2f5dc7940f5c414c8
meat <noskap@outlook.com>
2026-05-10 20:24:50 +0930

    use colonel gw for quotes

--- src/lib/quotes.ts
+++ src/lib/quotes.ts
@@ -1,25 +1,6 @@
-/** Celebration lines; {{entity}} is replaced with ENTITY_NAME from config */
-const RAW_QUOTES = [
-    "Certified {{entity}} moment.",
-    "This {{entity}} goes hard. Respect.",
-    "The timeline needed this {{entity}}.",
-    "Skill issue for everyone else. Not you. You have {{entity}}.",
-    "Local {{entity}} obtained. The prophecy continues.",
-    "That was fast. Almost suspiciously fast. Almost… {{entity}}.",
-    "Science cannot explain this {{entity}}.",
-    "They said it couldn't be done. They were wrong. {{entity}}.",
-    "You wouldn't download a {{entity}}. Oh wait, you just did.",
-    "Achievement unlocked: touched grass… then caught {{entity}}.",
-    "{{entity}} acquisition protocol: success.",
-    "The council has awarded you one (1) shiny {{entity}}.",
-    "Photographic evidence incoming. This {{entity}} is real.",
-    "That wasn't luck. That was raw {{entity}} energy.",
-    "Hideo would be proud. Probably. Maybe. {{entity}}.",
-    "You clicked/typed so fast the universe blinked. {{entity}} secured.",
-    "Another {{entity}} for the pile. The pile grows stronger.",
-    "Do not eat the {{entity}}. (Unless the rules say you can.)",
-    "If you tell anyone about this {{entity}}, they'll believe you.",
-];
+import { messages as RAW_QUOTES } from "../../colonel/src/data/messages.ts";
+
+/** Celebration lines from colonel-gw; {{entity}} is replaced when present */
 
 const SILLY_EXTRAS = [
     "Anyway, water your plants.",
